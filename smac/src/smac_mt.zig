dafd6aeac832c59042f86eeb26b6a0d8dc4700a2
Arjit Geo <143602102+arjitGeo@users.noreply.github.com>
2023-10-09 12:52:47 +0530

    Update smac_mt.zig for version 0.12.0

--- smac/src/smac_mt.zig
+++ smac/src/smac_mt.zig
@@ -1,7 +1,6 @@
 const std = @import("std");
 const rnd_gen = std.rand.DefaultPrng;
 const heap_alloc = std.heap.page_allocator;
-const stdout = std.io.getStdOut().writer();
 
 pub fn main() !void {
     const x = 4;
@@ -12,22 +11,20 @@ pub fn main() !void {
     while (i < x) : (i += 1) {
         list[i] = std.ArrayList(u8).init(heap_alloc);
         defer list[i].deinit();
-        t[i] = try std.Thread.spawn(.{}, count_it, .{ @truncate(u32, i), &list[i] });
+        t[i] = try std.Thread.spawn(.{}, count_it, .{ @as(u32, @truncate(i)), &list[i] });
     }
 
-    //we join all te threads to wait till they're done counting
     i = 0;
     while (i < x) : (i += 1) {
         t[i].join();
         try print_list(&list[i]);
-        //let's clean and free the indiviual list after copying
-        //because we also have immediate control, you gotta love Zig
         list[i].clearAndFree();
     }
 }
 
 fn print_list(list: *std.ArrayList(u8)) !void {
     var iter = std.mem.split(u8, list.items, "\n");
+    const stdout = std.io.getStdOut().writer();
     while (iter.next()) |item| {
         try stdout.print("{s}\n", .{item});
     }
